19d2a3e8ed440c0d1803a1321d598c0c34d21231
RuiLewis <s19c1062nh@s.chibakoudai.jp>
2023-01-21 00:09:32 +0900

    Test service

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,14 +1,33 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.srv import Query
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+def main():
+    rclpy.init()
+    node = Node("listener")
+    client = node.create_client(Query, 'query')
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('Waiting')
 
-rclpy.init()
-node = Node("listener")
-pub = node.create_subscription(Int16, "countup", cb, 1)
-rclpy.spin(node)
+    req = Query.Request()
+    req.name = "Rui"
+    future = client.call_async(req)
+
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if future.done():
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('Call failed')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+            break
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
 
 
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,21 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.srv import Query
+
+def cb(request, response):
+    if request.name == "Rui":
+        response.age = 22
+    else:
+        response.age = 0
+    
+    return response
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 1)
-
-n = 0
-
-def cb():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
-
-node.create_timer(0.5, cb)
+srv = node.create_service(Query, "query", cb)
 rclpy.spin(node)
 
 
